6bae15e50f75f63001e0de5b655257584ed5c78e
Sandeep.D <sandeep.d@simplilearn.net>
2022-03-10 21:09:26 +0530

    commit changes

---
+++ TestProject1/src/SumoftwoNumbers.java
@@ -0,0 +1,19 @@
+
+public class SumoftwoNumbers {
+
+	public static void main(String[] args) {
+		int a =20,b=25;
+		int sum=0;
+		sum=a+b;
+		System.out.println("Enter the value of a\n"+a);
+		System.out.println("Enter the value of b\n"+b);
+		System.out.println("The sum of two numbers a and b" +sum);
+		
+		
+		
+		
+	
+
+	}
+
+}
